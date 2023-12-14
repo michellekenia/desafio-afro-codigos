ec64b2cd4be50ee1024664d9d7d51d65dec373cf
michellekenia <keniamsoaress@gmail.com>
2023-12-14 10:51:37 -0300

    criar funcao matriz de numeros

---
+++ script.js
@@ -0,0 +1,30 @@
+function diferencaParesImpares(matriz) {
+  
+    let somaPares = 0;
+    let somaImpares = 0;
+  
+    for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        
+        
+  if (matriz[i][j] % 2 === 0) {
+          somaPares += matriz[i][j];
+        } 
+        
+  else {
+          somaImpares += matriz[i][j];
+        }
+      }
+    }
+  
+    return somaPares - somaImpares;
+  }
+  
+  const matriz = [
+    [2, 4, 9],
+    [3, 5, 7],
+    [10, 11, 8]];
+  
+  const result = diferencaParesImpares(matriz);
+  
+  console.log(result);
